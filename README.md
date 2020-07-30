9b5c942cf18749ea8461a0308368cfa0e4c961e9
adrianhopebailie <adrian@hopebailie.com>
2020-07-30 15:52:08 +0200

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
-# investec
-A CLI tool for using the Investec Open Banking APIs
+# Investec Open Banking CLI
+
+An interactive CLI application for communicating with the
+[Investec Open Banking APIs](https://developer.investec.com/programmable-banking/#open-api).
+
+Built with [Deno](https://deno.land/).
+
+## Install
+
+Assuming you already have deno [installed](https://deno.land/#installation).
+
+```sh
+deno install --allow-net=openapi.investec.com --allow-env https://raw.githubusercontent.com/adrianhopebailie/investec/master/investec.md
+```
+
+## Usage
+
+To use the application you'll need your client id and client secret from
+Investec.
+
+1.  Login to Investec Online.
+2.  Navigate to the Programmable Banking landing page.
+3.  Select the Open API tab.
+4.  Select the Enroll button.
+
+To run the application simply run:
+
+```sh
+$ investec
+```
+
+You can type `help` to get a list of available commands or `quit` to... quit.
+
+### Login
+
+The application will try to create an authenticated session when running any
+command if one doesn't already exist. However you can explicitly login using the
+`login` command.
+
+The provided credentials will be kept in memory as long as the application is
+running.
+
+To skip typing in the login credentials these can be provided as environment
+variables called `INVESTEC_CLIENT_ID` and `INVESTEC_CLIENT_SECRET`.
+
+```sh
+$ INVESTEC_CLIENT_ID=uytfqwed76r5quwdtfo86twef INVESTEC_CLIENT_SECRET=98uy978y23ry8 investec
+```
+
+### Accounts
+
+Using the `accounts` or `accts` command will list your accounts and prompt you
+to select an account that will be used for further operations such as
+transaction and balance enquiries. You can skip setting a selected account by
+simply hitting ⏎ (Enter).
+
+### Transactions
+
+The `transactions` and `txs` command will get a list of transactions for the
+currently selected account and prompt to select an account if one hasn't already
+been selected.
+
+### Balance
+
+The `balance` and `bal` command will get the current balance for the currently
+selected account and prompt to select an account if one hasn't already been
+selected.
+
+## TODO
+
+- [ ] Auto-fetch balances when getting account list
+- [ ] Better management of credential state (secure storage?)
+- [ ] Use currency from balance query for tx list
---
+++ investec.ts
@@ -0,0 +1,485 @@
+import * as colors from "https://deno.land/std/fmt/colors.ts";
+import { Table } from "https://deno.land/x/tbl/mod.ts";
+
+const BASE_URL = "https://openapi.investec.com";
+const INVESTEC_CLIENT_ID = "INVESTEC_CLIENT_ID";
+const INVESTEC_CLIENT_SECRET = "INVESTEC_CLIENT_SECRET";
+const ICON = "💸";
+const LOCKED = "🔐";
+const UNLOCKED = "🔓";
+const KEY = "🔑";
+
+class State {
+  public clientId? = Deno.env.get(INVESTEC_CLIENT_ID);
+  public clientSecret? = Deno.env.get(INVESTEC_CLIENT_SECRET);
+  public accessToken?: string = undefined;
+  public accessTokenExpiry = -1;
+  public selectedAccount?: Account = undefined;
+  public get isLoggedIn(): boolean {
+    return this.accessToken !== undefined &&
+      (this.accessTokenExpiry > Date.now());
+  }
+}
+
+interface FetchAccessTokenResponse {
+  access_token: string;
+  token_type: "Bearer";
+  expires_in: number;
+  scope: "accounts";
+}
+
+interface Account {
+  accountId: string;
+  accountNumber: string;
+  accountName: string;
+  referenceName: string;
+  productName: string;
+}
+
+interface Transaction {
+  accountId: string;
+  type: string;
+  status: string;
+  description: string;
+  cardNumber: string;
+  postingData: string;
+  valueDate: string;
+  actionDate: string;
+  amount: number;
+}
+
+interface FetchAccountsResponse {
+  data: {
+    accounts: Account[];
+  };
+  links: {
+    self: string;
+  };
+  meta: {
+    totalPages: number;
+  };
+}
+
+interface FetchAccountTransactionsResponse {
+  data: {
+    transactions: Transaction[];
+  };
+  links: {
+    self: string;
+  };
+  meta: {
+    totalPages: number;
+  };
+}
+
+interface FetchAccountBalanceResponse {
+  data: {
+    accountId: string;
+    currentBalance: number;
+    availableBalance: number;
+    currency: string;
+  };
+  links: {
+    self: string;
+  };
+  meta: {
+    totalPages: number;
+  };
+}
+
+interface AccountRow {
+  " ": string;
+  "Account Name": string;
+  "Account Number": string;
+  "Product": string;
+}
+
+interface TransactionRow {
+  "Date": string;
+  "Status": string;
+  "Description": string;
+  "Card Number": string;
+  "Value Date": string;
+  "Amount": string;
+}
+
+async function readLineFromConsole(): Promise<string> {
+  const buf = new Uint8Array(1024);
+  const n = <number> await Deno.stdin.read(buf);
+  return new TextDecoder().decode(buf.subarray(0, n)).trim();
+}
+
+async function getInput(message: string = ""): Promise<string> {
+  await Deno.stdout.write(new TextEncoder().encode(message + ": "));
+  return await readLineFromConsole();
+}
+
+async function prompt(state: State): Promise<string[]> {
+  const account = (state.selectedAccount)
+    ? ` ${
+      colors.green(state.selectedAccount.accountNumber)
+    }/${state.selectedAccount.accountName}/${
+      colors.green(state.selectedAccount.productName)
+    } `
+    : " ";
+  await Deno.stdout.write(
+    new TextEncoder().encode(
+      `${ICON}${account}${(state.isLoggedIn) ? LOCKED : UNLOCKED} > `,
+    ),
+  );
+  return (await readLineFromConsole()).split(" ");
+}
+
+function basicAuthToken(state: State) {
+  return btoa(`${state.clientId}:${state.clientSecret}`);
+}
+
+async function fetchAccessToken(
+  authToken: string,
+): Promise<FetchAccessTokenResponse | undefined> {
+  console.log("Logging in...");
+
+  const res = await fetch(BASE_URL + "/identity/v2/oauth2/token", {
+    method: "POST",
+    headers: {
+      Authorization: `Basic ${authToken}`,
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: "grant_type=client_credentials&scope=accounts",
+  });
+
+  if (res.ok) {
+    return await res.json() as FetchAccessTokenResponse;
+  } else {
+    return undefined;
+  }
+}
+
+async function fetchAccounts(
+  state: State,
+): Promise<FetchAccountsResponse | undefined> {
+  if (!state.isLoggedIn) {
+    await login(state);
+  }
+
+  const res = await fetch(BASE_URL + "/za/pb/v1/accounts", {
+    headers: {
+      Authorization: `Bearer ${state.accessToken}`,
+      "Accept": "application/json",
+    },
+  });
+
+  if (res.ok) {
+    return await res.json() as FetchAccountsResponse;
+  } else {
+    return undefined;
+  }
+}
+
+async function fetchAccountTransactions(
+  state: State,
+  accountId: string,
+): Promise<FetchAccountTransactionsResponse | undefined> {
+  if (!state.isLoggedIn) {
+    await login(state);
+  }
+  const res = await fetch(
+    BASE_URL + `/za/pb/v1/accounts/${accountId}/transactions`,
+    {
+      headers: {
+        Authorization: `Bearer ${state.accessToken}`,
+        "Accept": "application/json",
+      },
+    },
+  );
+  if (res.ok) {
+    return await res.json() as FetchAccountTransactionsResponse;
+  } else {
+    return undefined;
+  }
+}
+
+async function fetchAccountBalance(
+  state: State,
+  accountId: string,
+): Promise<FetchAccountBalanceResponse | undefined> {
+  if (!state.isLoggedIn) {
+    await login(state);
+  }
+  const res = await fetch(
+    BASE_URL + `/za/pb/v1/accounts/${accountId}/balance`,
+    {
+      headers: {
+        Authorization: `Bearer ${state.accessToken}`,
+        "Accept": "application/json",
+      },
+    },
+  );
+  if (res.ok) {
+    return await res.json() as FetchAccountBalanceResponse;
+  } else {
+    return undefined;
+  }
+}
+
+async function login(state: State, resetClientCredentials = false) {
+  if (
+    state.clientId == undefined ||
+    state.clientSecret == undefined ||
+    resetClientCredentials
+  ) {
+    //TODO: If resetClientCredentials and env vars are set then fallback to these
+    state.clientId = await getInput(`Enter Client ID ${KEY} `);
+    state.clientSecret = await getInput(`Enter Client Secret ${KEY} `);
+  }
+  const tokenResponse = await fetchAccessToken(
+    basicAuthToken(state),
+  );
+  if (tokenResponse !== undefined) {
+    state.accessToken = tokenResponse.access_token;
+    state.accessTokenExpiry = (tokenResponse.expires_in * 1000) + Date.now();
+    console.log("Successfully logged in");
+  } else {
+    console.log("Error logging in");
+  }
+}
+
+async function logout(state: State) {
+  state.accessToken = undefined;
+  state.accessTokenExpiry = -1;
+}
+
+async function getAccounts(state: State) {
+  if (!state.isLoggedIn) {
+    await login(state);
+  }
+
+  const result = await fetchAccounts(state);
+  if (result) {
+    const accounts = result.data.accounts;
+    const accountRows: AccountRow[] = [];
+    accounts.map((account, i) => {
+      accountRows.push({
+        " ": i.toString(),
+        "Account Number": account.accountNumber,
+        "Account Name": account.accountName,
+        "Product": account.productName,
+      });
+    });
+    const table = new Table({
+      header: [
+        " ",
+        "Account Number",
+        "Account Name",
+        "Product",
+      ],
+    });
+    table.fromObjects(accountRows);
+    console.log(table.toString());
+
+    if (accountRows.length === 0) {
+      state.selectedAccount = undefined;
+    }
+
+    if (accountRows.length === 1) {
+      state.selectedAccount = accounts[1];
+    }
+
+    let selectedAccountIndex = NaN;
+    while (isNaN(selectedAccountIndex)) {
+      const selection = await getInput(
+        `Select account (0 - ${accounts.length - 1}) or ENTER to cancel`,
+      );
+      if (selection === "") {
+        state.selectedAccount = undefined;
+        return;
+      } else {
+        selectedAccountIndex = Number.parseInt(selection);
+        if (
+          isNaN(selectedAccountIndex) || selectedAccountIndex < 0 ||
+          selectedAccountIndex >= accounts.length
+        ) {
+          console.log(
+            `Invalid selection. Please provide a number between 0 and ${accounts
+              .length - 1}`,
+          );
+        } else {
+          state.selectedAccount = accounts[selectedAccountIndex];
+        }
+      }
+    }
+  } else {
+    console.log(colors.red("Error fetching accounts"));
+  }
+}
+
+async function getTransactionsForSelectedAccount(state: State) {
+  if (!state.isLoggedIn) {
+    await login(state);
+  }
+
+  if (!state.selectedAccount) {
+    await getAccounts(state);
+    if (!state.selectedAccount) {
+      console.log(colors.red(`No account selected`));
+      return;
+    }
+  }
+
+  const result = await fetchAccountTransactions(
+    state,
+    state.selectedAccount.accountId,
+  );
+  if (result) {
+    const txs = result.data.transactions;
+    const txRows: TransactionRow[] = [];
+    txs.map((tx, i) => {
+      txRows.push({
+        "Date": tx.actionDate,
+        "Description": tx.description,
+        "Amount": "ZAR" +
+          ((tx.type === "DEBIT" ? "-" : "+") + tx.amount.toFixed(2)).padStart(
+            12,
+            " ",
+          ), // TODO: Get currency from balance request
+        "Status": tx.status,
+        "Card Number": tx.cardNumber,
+        "Value Date": tx.valueDate,
+      });
+    });
+    const table = new Table({
+      header: [
+        "Date",
+        "Description",
+        "Amount",
+        "Status",
+        "Card Number",
+        "Value Date",
+      ],
+    });
+    table.fromObjects(txRows);
+    console.log(table.toString());
+  } else {
+    console.log("Error fetching accounts");
+  }
+}
+
+async function getBalanceForSelectedAccount(state: State) {
+  if (!state.isLoggedIn) {
+    await login(state);
+  }
+
+  if (!state.selectedAccount) {
+    await getAccounts(state);
+    if (!state.selectedAccount) {
+      console.log(colors.red(`No account selected`));
+      return;
+    }
+  }
+
+  const balance = await fetchAccountBalance(
+    state,
+    state.selectedAccount.accountId,
+  );
+  if (balance) {
+    console.log(
+      `Current Balance: ${balance.data.currency} ${balance.data.currentBalance}`,
+    );
+    console.log(
+      `Available Balance: ${balance.data.currency} ${balance.data.availableBalance}`,
+    );
+  } else {
+    console.log("Error fetching balance");
+  }
+}
+
+async function help(error?: string) {
+  console.log(`
+
+
+  ${colors.yellow("login [--reset]")}
+      | Login to Open Banking using a client id and secret (required before executing any other command)
+      | (Looks for INVESTEC_CLIENT_ID and INVESTEC_CLIENT_SECRET from the env and prompts for them if they are not found)
+      |
+      | --reset 
+      |          Clear the client id and secret in memory and prompt for new credentials
+
+  ${colors.yellow("accounts | accts")}
+      | List accounts.
+      | (Optionally select an account for further operations)
+
+  ${colors.yellow("transactions | txs")}
+      | List transactions for the currently selected account.
+
+  ${colors.yellow("balance | bal")}
+      | Get balances for the currently selected account.
+
+  ${colors.yellow("help")}
+      | Displays this help information.
+
+  ${colors.yellow("quit")}
+      | Quit the application.
+
+`.trim());
+  if (error) {
+    console.log(colors.red(error));
+  }
+}
+
+const state = new State();
+console.log(
+  `
+${colors.yellow("Investec Open Banking REPL")}
+Copyright 2020 - Adrian Hope-Bailie
+Licensed under an Apache 2.0 license
+
+Type 'help' for a list of available commands
+`,
+);
+
+while (true) {
+  const input = await prompt(state);
+  const command = input[0];
+  switch (command) {
+    case "login":
+      const reset = input[1] !== undefined && input[1] === "--reset";
+      await login(state, reset);
+      break;
+
+    case "logout":
+      await logout(state);
+      break;
+
+    case "accounts":
+    case "accts":
+      await getAccounts(state);
+      break;
+
+    case "transactions":
+    case "txs":
+      await getTransactionsForSelectedAccount(state);
+      break;
+
+    case "balance":
+    case "bal":
+      await getBalanceForSelectedAccount(state);
+      break;
+
+    case "quit":
+      Deno.exit(0);
+
+    case "help":
+      await help();
+      break;
+
+    case "":
+      break;
+
+    default:
+      if (command !== undefined) {
+        console.log(`${colors.red(command)} is not a valid command\n`);
+      }
+      await help();
+      break;
+  }
+}
